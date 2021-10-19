649a2e4e48ea4056be848dcb305faf0283454e3f
rootn3rd <krishnahare14@gmail.com>
2021-10-19 10:27:44 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# web-platform-ukhaeq
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/web-platform-ukhaeq)
---
+++ script.js
@@ -0,0 +1,6 @@
+console.log('hello!');
+setTimeout(()=>{
+  var skills = document.querySelector('.skills');
+  console.log(skills);
+
+},1000);
